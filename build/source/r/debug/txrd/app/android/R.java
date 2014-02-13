19ce48ab61411929d87c01198345d9e4c9aab6e2
mollyrand <molly.rand@gmail.com>
2014-02-12 22:02:16 -0600

    determined schedule issue - Webview scheduleView remains null at/beyond line 30.  But why?

--- build/source/r/debug/txrd/app/android/R.java
+++ build/source/r/debug/txrd/app/android/R.java
@@ -22,12 +22,11 @@ public final class R {
         public static final int sched_2014=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
+        public static final int action_settings=0x7f090004;
         public static final int buttonPhotos=0x7f090003;
         public static final int buttonTeams=0x7f090002;
         public static final int imageButtonSched=0x7f090000;
         public static final int imageButtonTix=0x7f090001;
-        public static final int scheduleCard=0x7f090004;
     }
     public static final class layout {
         public static final int activity_all_teams=0x7f030000;
--- src/txrd/app/android/MainActivity.java
+++ src/txrd/app/android/MainActivity.java
@@ -12,8 +12,7 @@ import android.webkit.WebView;
 //import android.widget.Button;
 
 public class MainActivity extends Activity {
-	
-	WebView scheduleView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +21,19 @@ public class MainActivity extends Activity {
         
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
     public void openScheduleLink(View v) { 
-    	//2.9.14 - should now open up a webview to display schedule card image
-//        dammit, why do you keep killing the app?? are you too big?
+//  currently: shows schedulecard image
+//  future: integrate with calendar?
         Intent intent = new Intent(this, ScheduleView.class);
         startActivity(intent);
-    	scheduleView = (WebView)findViewById(R.id.scheduleCard);
-	    scheduleView.getSettings().setBuiltInZoomControls(true);
-	    scheduleView.loadUrl("file:///android_R/drawable/sched_2014.jpg");
+
     	
     }
     public void openTicketsLink(View v) { 
@@ -39,23 +43,8 @@ public class MainActivity extends Activity {
 	    i.setData(Uri.parse(url)); 
 	    startActivity(i); 
 }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-	    // Check if the key event was the Back button and if there's history
-	    if ((keyCode == KeyEvent.KEYCODE_BACK) && scheduleView.canGoBack()) {
-	        scheduleView.goBack();
-	        return true;
-	    }
-	    // If it wasn't the Back key or there's no web page history, bubble up to the default
-	    // system behavior (probably exit the activity)
-	    return super.onKeyDown(keyCode, event);
-	}
+
+
 
     public void seeTeams(View view) {
     	Intent intent = new Intent(this, AllTeams.class);
